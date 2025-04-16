27a99d7c632043b833c7b40fcc4ec92aff9fc7c0
Neto-Pereira25 <devneto203@gmail.com>
2025-04-16 15:33:37 -0300

    button to start the pomodoro (working)

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { JSX } from 'react/jsx-runtime';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
@@ -13,26 +13,41 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+
+  useEffect(() => {
+    if (working) {
+      document.body.classList.add('working');
+    } else {
+      //
+    }
+  }, [working]);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000);
+  }, timeCounting ? 1000 : null);
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  }
 
   return (
     <div className='pomodoro'>
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
+        <Button text='Work' onClick={() => configureWork()}></Button>
         <Button text='teste' onClick={() => console.log(1)}></Button>
-        <Button text='teste' onClick={() => console.log(1)}></Button>
-        <Button text='teste' onClick={() => console.log(1)}></Button>
+        <Button text={timeCounting ? 'Pause' : 'Play'} onClick={() => setTimeCounting(!timeCounting)}></Button>
       </div>
 
       <div className="details">
-        <p>Testando: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente commodi quae quisquam, corrupti in ipsa magnam voluptates magni nemo excepturi labore odio reiciendis. Quibusdam accusamus aliquid officia. Mollitia, ex delectus.</p>
-        <p>Testando: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente commodi quae quisquam, corrupti in ipsa magnam voluptates magni nemo excepturi labore odio reiciendis. Quibusdam accusamus aliquid officia. Mollitia, ex delectus.</p>
-        <p>Testando: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente commodi quae quisquam, corrupti in ipsa magnam voluptates magni nemo excepturi labore odio reiciendis. Quibusdam accusamus aliquid officia. Mollitia, ex delectus.</p>
-        <p>Testando: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente commodi quae quisquam, corrupti in ipsa magnam voluptates magni nemo excepturi labore odio reiciendis. Quibusdam accusamus aliquid officia. Mollitia, ex delectus.</p>
+        <p>Testando: Lorem ipsum dolor</p>
+        <p>Testando: Lorem ipsum dolor</p>
+        <p>Testando: Lorem ipsum dolor</p>
+        <p>Testando: Lorem ipsum dolor</p>
       </div>
     </div>
   );
