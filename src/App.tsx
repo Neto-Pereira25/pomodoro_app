5434339e33ed8ac2a87fc22c5ed27cd3f007fadb
Neto-Pereira25 <devneto203@gmail.com>
2025-04-16 16:50:36 -0300

    corrected time for each pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App(): JSX.Element {
   return (
     <div className="container">
       <PomodoroTimer
-        pomodoroTime={10}
-        shortRestTime={2}
-        longRestTime={5}
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
         cycles={4}
       />
     </div>
